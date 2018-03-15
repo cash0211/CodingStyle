1980c1d4fe69597f0d58e7b59563fe0e9fbd172c
cash <cashxuxu@gmail.com>
2018-03-15 14:34:51 +0800

    Initial Commit

---
+++ CodingStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodingStyle
+//
+//  Created by cash on 15/03/2018.
+//  Copyright © 2018 cash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodingStyle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodingStyle
+//
+//  Created by cash on 15/03/2018.
+//  Copyright © 2018 cash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
